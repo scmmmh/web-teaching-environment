37c227044d5da614cdde3ebcd882db1c5d6d6e86
Mark <Mark.Hall@work.room3b.eu>
2016-07-01 18:41:22 +0100

    Code cleanup.

--- src/wte/helpers/__init__.py
+++ src/wte/helpers/__init__.py
@@ -4,18 +4,9 @@
 :mod:`wte.helpers` -- Helper functionality for views
 ####################################################
 
-The :mod:`~wte.helpers` module imports various helper modules that can be used
-in the templates via the ``h`` variable. For details of the functions available
-consulte the following documentation: :mod:`pywebtools.text`,
-:mod:`wte.helpers.auth`, and :mod:`wte.helpers.form`.
+The :mod:`~wte.helpers` package provides utility functionality
+via the :mod:`~wte.helpers.frontend`.
 
 .. moduleauthor:: Mark Hall <mark.hall@work.room3b.eu>
 """
-import asset
-
 from . import frontend  # NOQA
-
-
-def version():
-    """Return the current version number."""
-    return asset.version('WebTeachingEnvironment')
